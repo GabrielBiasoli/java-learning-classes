3417bc17cf444e9de3e4ce795311c52f9026090c
GabrielBiasoli <bielbiasoli33@icloud.com>
2025-03-17 10:48:18 -0300

    feat: add polymorphism example to Principal class

--- Polimorfismo/src/empresa/Principal.java
+++ Polimorfismo/src/empresa/Principal.java
@@ -3,8 +3,20 @@ package empresa;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Funcionario funcionarios[] = {	new Assalariado("Mário", 3500),
+										new Horista("Luigi", 100, 40.5f),
+										new Comissionado("Yoshi", 50000, 0.05f)};
+		
+		Funcionario f;
+		float total = 0;
+		for (int i = 0; i < funcionarios.length; i++) {
+			f = funcionarios[i];
+			System.out.println(f.nome + " - salário: " + f.pagamento());
+			total += f.pagamento();
+		}
+		
+		System.out.println("Total: R$" + total);
 	}
 
 }
